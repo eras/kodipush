4161337dffaa3b8646994a5c96c3aa627ef4f370
Erkki Seppälä <flux@modeemi.fi>
2016-07-23 11:50:00 +0300

    KodiPush: support for sending the URL to Kodi

--- kodipush.ml
+++ kodipush.ml
@@ -7,7 +7,7 @@ let dump uri meth headers =
   Printf.printf "Uri: %s\nMethod: %s\nHeaders\nHeaders: %s\n%!"
     uri meth headers
 
-let server external_address filename =
+let server external_address filename kodi_address =
   let callback _conn req body =
     let uri = req |> Request.uri |> Uri.to_string in
     let meth = req |> Request.meth in
@@ -20,8 +20,20 @@ let server external_address filename =
     >>= (fun (headers_out, status, body) -> Server.respond ~headers:headers_out ~status ~body ())
   in
   let port = 8000 in
-  Printf.printf "http://%s:%d/%s\n%!" external_address port (Filename.basename filename);
-  Server.create ~mode:(`TCP (`Port port)) (Server.make ~callback ())
+  let url = Printf.sprintf "http://%s:%d/%s" external_address port (Filename.basename filename) in
+  let server_done = Lwt_mvar.create_empty () in
+  Printf.printf "%s\n%!" url;
+  Lwt.async (fun () ->
+      Server.create ~mode:(`TCP (`Port port)) (Server.make ~callback ()) >>= fun () ->
+      Lwt_mvar.put server_done ()
+    );
+  Lwt_unix.sleep 0.1 >>= fun () ->
+  Kodi.player_open ("http://" ^ kodi_address) url >>= fun () ->
+  Lwt_mvar.take server_done
+
+let kodi_address =
+  let doc = "Address of the Kodi server" in
+  Arg.(value & opt string "xbmc:8080" & info ["k"; "kodi"] ~docv:"KODI" ~doc)
 
 let external_address =
   let doc = "External address of the server" in
@@ -33,7 +45,7 @@ let filename =
   Arg.(required & pos ~rev:true 0 (some string) None & info [] ~docv:"FILENAME" ~doc)
 
 let args =
-  Term.(const server $ external_address $ filename)
+  Term.(const server $ external_address $ filename $ kodi_address)
 
 let info = Term.info "kodipush"
 
