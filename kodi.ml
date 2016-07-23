2fb20f7aaf73300b36170f6c84a1820412420d01
Erkki Seppälä <flux@modeemi.fi>
2016-07-23 13:14:25 +0300

    Kodi.player_stop: added

--- kodi.ml
+++ kodi.ml
@@ -2,20 +2,21 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
-let jsonrpc params =
+let jsonrpc method_ params =
   `Assoc [
     ("jsonrpc", `String "2.0");
-    ("method", `String "Player.Open");
+    ("method", `String method_);
     ("params", params);
     ("id", `Int 1)
   ]
 
-let request kodi_host params =
-  let json = jsonrpc params in
+let request kodi_host method_ params =
+  let json = jsonrpc method_ params in
 
   let headers = Header.init () in
   let headers = Header.add headers "Content-Type" "application/json" in
   let body = Lwt_stream.of_list [Yojson.Basic.to_string json] |> Cohttp_lwt_body.of_stream in
+  Printf.printf "Request: %s\n%!" (Yojson.Basic.to_string json);
   Client.post ~headers ~body (Uri.of_string (kodi_host ^ "/jsonrpc")) >>= fun (resp, body) ->
   let code = resp |> Response.status |> Code.code_of_status in
   Printf.printf "Response code: %d\n%!" code;
@@ -25,10 +26,14 @@ let request kodi_host params =
   ()
 
 let player_open kodi_host url =
-  request kodi_host  (`Assoc [
+  request kodi_host "Player.Open"  (`Assoc [
       ("item", `Assoc [
           ("file", `String url)
         ])
     ])
+
+let player_stop kodi_host =
+  request kodi_host "Player.Stop"  (`Assoc [
+      ("playerid", `Int 2)])
 
 (* curl -H 'Content-Type: application/json' --data-binary '{ "jsonrpc": "2.0", "method": "Player.Open", "params": { "item": { "file": "'"${1}"'" } }, "id": 1 }' "${XBMC_HOST}/jsonrpc" *)
